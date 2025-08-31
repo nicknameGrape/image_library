b6d7cbcbc227fbe0d25cabb7f4666a0e264fcc7f
GR <kurumeteacher@gmail.com>
2025-08-31 23:58:25 +0900

    added character silhouette quizzes to leeches

---
+++ characters/popular.mjs
@@ -0,0 +1,20 @@
+const POPULAR = [
+	{"src": "barbie.png", "name": "Barbie"},
+	{"src": "curious_george.png", "name": "Curious George"},
+	{"src": "dokinchan.gif", "name": "Dokkinchan"},
+	{"src": "donkey_kong.png", "name": "Donkey Kong"},
+	{"src": "garigari.png", "name": "Garigari-kun"},
+	{"src": "goku.png", "name": "Son Goku"},
+	{"src": "hanakappa.png", "name": "Hanakappa"},
+	{"src": "himawari.png", "name": "Himawari"},
+	{"src": "jaiko.png", "name": "Jaiko"},
+	{"src": "kaguyahime.png", "name": "Kaguyahime"},
+	{"src": "peach.png", "name": "Princess Peach"},
+	{"src": "ronald_mcdonald.png", "name": "Ronald McDonald"},
+	{"src": "sazae.png", "name": "Sazae-san"},
+	{"src": "sonic.png", "name": "Sonic"},
+	{"src": "waldo.gif", "name": "Waldo"},
+	{"src": "zelda.png", "name": "Zelda"}
+];
+
+export default POPULAR;
